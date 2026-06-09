e91bfb6ae263e9a136e52a6cbf156f839e6b245e
Rabindra nath mahato <mahato.rabindra.rm@gmail.com>
2026-06-09 23:55:11 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let ana = new Map()
+        for (let i = 0; i < strs.length; i++) {
+            var ele = strs[i].split('').sort().join()
+            if (ana.has(ele)) {
+               ana.get(ele).push(strs[i])
+            }
+            else {
+                ana.set(ele, [strs[i]])
+            }
+        }
+
+        let ans = [];
+        for(let [key , value] of ana){
+            ans.push(value)
+        }
+        return ans;
+    }
+}
