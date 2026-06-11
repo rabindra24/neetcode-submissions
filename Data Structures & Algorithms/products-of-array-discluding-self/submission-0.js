b1cd2a2abc0f6fa854d9629da0d936554275391d
Rabindra nath mahato <mahato.rabindra.rm@gmail.com>
2026-06-11 17:47:36 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let arr = []
+        for(var i = 0; i < nums.length ; i++){
+            let cal = 1;
+            for(var j = 0; j < nums.length; j++){
+                if(i !== j){
+                    cal *= nums[j]
+                }
+            }
+            arr.push(cal)
+        }
+        console.log(arr)
+        return arr;
+    }
+}
