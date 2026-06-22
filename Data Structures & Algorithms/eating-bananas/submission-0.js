a97d31616bfbec3bada21421d5780c906d78da9a
Rabindra nath mahato <mahato.rabindra.rm@gmail.com>
2026-06-22 22:45:11 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+    let left = 1;
+    let right = Math.max(...piles);
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        let hours = 0;
+        for (let pile of piles) {
+            hours += Math.ceil(pile / mid);
+        }
+        if (hours <= h) {
+            right = mid; // try smaller speed
+        } else {
+            left = mid + 1; // need faster speed
+        }
+    }
+    return left;
+    }
+}
